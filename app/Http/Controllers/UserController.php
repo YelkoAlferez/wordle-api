0ca8811e10c291f67eecacd94e11b1a838457b24
YelkoAlferez <alferezyelko@gmail.com>
2024-10-17 19:16:40 +0200

    create login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Auth;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function login(Request $request){
-
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            return response()->json(["retCode" => 200, "name" => auth()->user()->name]);
+        }else{
+            return response()->json(["retCode" => 404]);
+        }
     }
 
     public function register(Request $request){
